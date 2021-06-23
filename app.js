2491a1b81827d602f4177a863931bc63fc51194e
Misha Krasnonos <muzishutka7@gmail.com>
2021-06-23 11:50:24 +0300

    добавил разметку по шаблонам Layout

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const exhbs = require('express-handlebars');
+
 const app = express();
-app.listen(7, () => {
-  console.log(`good!, running on port ${7}`);
+
+app.use(express.static('public'));
+
+app.set('view engine', 'hbs');
+app.engine('hbs', exhbs({ extname: 'hbs' }));
+
+//Запросы пользователей
+//http://localhost:777/
+app.get('/', (req, res) => {
+  res.render('home');
+});
+//http://localhost:777/about
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.listen(777, () => {
+  console.log(`good!, running on port ${777}`);
 });
