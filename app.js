00093578d0c835a548636f1a7e79d6fa1d703fdf
Misha Krasnonos <muzishutka7@gmail.com>
2021-06-23 14:49:58 +0300

    APP-server by lesso Repeta completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exhbs = require('express-handlebars');
+const products = require('./products.json');
 
 const app = express();
 
@@ -11,17 +12,30 @@ app.engine('hbs', exhbs({ extname: 'hbs' }));
 //Запросы пользователей
 //http://localhost:777/
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', { cssFileName: 'home', titlePage: 'HOME' });
 });
 //http://localhost:777/about
 app.get('/about', (req, res) => {
-  res.render('about');
+  res.render('about', { cssFileName: 'about', titlePage: 'About' });
 });
 //http://localhost:777/about
 app.get('/products', (req, res) => {
-  res.render('products');
+  res.render('products', {
+    products,
+    cssFileName: 'products',
+    titlePage: 'Goods',
+  });
 });
-
+//http://localhost:777/product/ -- dynamic
+app.get('/product/:productId', (req, res) => {
+  const oneProduct = products.find(p => p.id === req.params.productId);
+  res.render('product', {
+    oneProduct,
+    titlePage: 'product',
+    cssFileName: 'onePro',
+  });
+});
+//local host
 app.listen(777, () => {
   console.log(`good!, running on port ${777}`);
 });
